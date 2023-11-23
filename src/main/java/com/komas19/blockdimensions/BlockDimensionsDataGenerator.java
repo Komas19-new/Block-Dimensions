2805b834dce15119bd78768bc68a0b15c01b4c46
Komas19-new <benoitbourcier@outlook.com>
2023-11-23 17:56:42 +0100

    Initial commit

---
+++ src/main/java/com/komas19/blockdimensions/BlockDimensionsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.komas19.blockdimensions;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BlockDimensionsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
