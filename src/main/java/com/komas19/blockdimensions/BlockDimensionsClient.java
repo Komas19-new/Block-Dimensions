08e273951252bbfc1f95d7efaba28c6ef26b8a54
Komas19-new <benoitbourcier@outlook.com>
2023-11-23 18:03:44 +0100

    client

---
+++ src/main/java/com/komas19/blockdimensions/BlockDimensionsClient.java
@@ -0,0 +1,10 @@
+package com.komas19.blockdimensions;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BlockDimensionsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
