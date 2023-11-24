dce058916b1670d6169a04baf50501984cecbd0f
Komas19-new <benoitbourcier@outlook.com>
2023-11-24 21:25:43 +0100

    REI compatibility :) -> https://github.com/shedaniel/RoughlyEnoughItems/tree/13.x-1.20.2

---
+++ src/main/java/com/komas19/blockdimensions/compat/BlockDimensionsREIClientPlugin.java
@@ -0,0 +1,23 @@
+package com.komas19.blockdimensions.compat;
+
+import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
+import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
+import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
+import me.shedaniel.rei.api.client.registry.screen.ScreenRegistry;
+
+public class BlockDimensionsREIClientPlugin implements REIClientPlugin {
+    @Override
+    public void registerCategories(CategoryRegistry registry) {
+        REIClientPlugin.super.registerCategories(registry);
+    }
+
+    @Override
+    public void registerDisplays(DisplayRegistry registry) {
+        REIClientPlugin.super.registerDisplays(registry);
+    }
+
+    @Override
+    public void registerScreens(ScreenRegistry registry) {
+        REIClientPlugin.super.registerScreens(registry);
+    }
+}
